f3e392fe71383a6e7e5f151665e5cd9bf4a5e764
onesecure <keysecuresale@gmail.com>
2021-11-15 05:43:16 +0000

    format issues

--- tweetpost.py
+++ tweetpost.py
@@ -32,18 +32,18 @@ def postTweet_py3(text, image):
 
 
 def postTweet_py2(text, image):
-	import tweetpony
-	api = tweetpony.API(consumer_key = consumer_key, consumer_secret = consumer_secret, access_token = access_token, access_token_secret = access_token_secret)
-	user = api.user
+    import tweetpony
+    api = tweetpony.API(consumer_key = consumer_key, consumer_secret = consumer_secret, access_token = access_token, access_token_secret = access_token_secret)
+    user = api.user
     result = True
-	try:
-		if image:
-			api.update_status_with_single_media(status = text, media=image)
-		else :
-			api.update_status(status = text)
-	except tweetpony.APIError as err:
-		print("Oops, something went wrong! Twitter returned error #%i and said: %s" % (err.code, err.description))
+    try:
+        if image:
+            api.update_status_with_single_media(status = text, media=image)
+        else :
+            api.update_status(status = text)
+    except tweetpony.APIError as err:
+        print("Oops, something went wrong! Twitter returned error #%i and said: %s" % (err.code, err.description))
         result = False
-	else:
-		pass #print "Yay! Your tweet has been sent!"
+    else:
+        pass #print "Yay! Your tweet has been sent!"
     return result
